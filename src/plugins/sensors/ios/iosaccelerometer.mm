6d05123edf6718f6d1149104df8be111d53d68f2
Richard Moe Gustavsen <richard.gustavsen@digia.com>
2013-03-18 15:07:20 +0100

    iOS: bugfix accelerometer timestamp
    Convert from NSTimeInterval to microseconds
    
    Change-Id: I51fb3791aa72dcc7514c39820e4347a3c1d4039a
    Reviewed-by: Thomas McGuire <thomas.mcguire@kdab.com>
    Reviewed-by: Lorn Potter <lorn.potter@jollamobile.com>

--- src/plugins/sensors/ios/iosaccelerometer.mm
+++ src/plugins/sensors/ios/iosaccelerometer.mm
@@ -86,7 +86,9 @@ char const * const IOSAccelerometer::id("ios.accelerometer");
                 qreal x = qreal(acc.x) * G * -1;
                 qreal y = qreal(acc.y) * G * -1;
                 qreal z = qreal(acc.z) * G * -1;
-                m_qiosAccelerometer->readingsChanged(quint64(data.timestamp), x, y, z);
+                // Convert from NSTimeInterval to microseconds:
+                quint64 timestamp = quint64(data.timestamp * 1000000);
+                m_qiosAccelerometer->readingsChanged(timestamp, x, y, z);
             });
         }];
     }
