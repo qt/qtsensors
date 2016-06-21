6221ea85ec76566125b9c5e427a66ff13e8f5867
Thiago Macieira <thiago.macieira@intel.com>
2016-06-21 14:39:28 -0700

    Use QDateTime::currentMSecsSinceEpoch() instead of currentDateTime
    More efficient if all you want is the number of seconds.
    
    Change-Id: Ib57b52598e2f452985e9fffd145a371003ede342
    Reviewed-by: Lorn Potter <lorn.potter@canonical.com>

--- src/plugins/sensors/simulator/simulatorirproximitysensor.cpp
+++ src/plugins/sensors/simulator/simulatorirproximitysensor.cpp
@@ -54,7 +54,7 @@ void SimulatorIRProximitySensor::poll()
     QtMobility::QIRProximityReadingData data = SensorsConnection::instance()->qtIRProximityData;
     quint64 newTimestamp;
     if (!data.timestamp.isValid())
-        newTimestamp = QDateTime::currentDateTime().toTime_t();
+        newTimestamp = QDateTime::currentMSecsSinceEpoch() / 1000;
     else
         newTimestamp = data.timestamp.toTime_t();
     if (m_reading.reflectance() != data.irProximity) {
