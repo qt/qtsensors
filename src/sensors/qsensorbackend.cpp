d8eef1687dbd7a39affaedc24f7faef320ef33bd
Paul Wicking <paul.wicking@qt.io>
2018-03-20 10:37:24 +0100

    Doc: Fix libclang related warning for templated method
    Corrected method signature in documentation.
    
    Change-Id: I86343f9e0bd4df04a4c24f6d7c02be663ebac0d9
    Reviewed-by: Topi Reiniö <topi.reinio@qt.io>

--- src/sensors/qsensorbackend.cpp
+++ src/sensors/qsensorbackend.cpp
@@ -147,7 +147,7 @@ QSensor *QSensorBackend::sensor() const
 }
 
 /*!
-    \fn QSensorBackend<T>::setReading(T *reading)
+    \fn template <typename T> T *QSensorBackend::setReading(T *reading)
 
     This function is called to initialize the \a reading
     classes used for a sensor.
